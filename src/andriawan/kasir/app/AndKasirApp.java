8c7620fe3c55d302098dc63d0baf7e5c4af91c46
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2018-01-25 22:10:58 +0700

    penambahan command via terminal untuk aksi migrasi database

--- src/andriawan/kasir/app/AndKasirApp.java
+++ src/andriawan/kasir/app/AndKasirApp.java
@@ -6,14 +6,19 @@
 package andriawan.kasir.app;
 
 import andriawan.kasir.controller.UserLoginController;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import utilities.ConnectionManager;
+import utilities.MigrationManager;
 
 
 /**
  *
  * @author andriawan
  *
- * Starting Point Aplikasi AndKasir v1
+ * Starting Point Aplikasi AndKasir 
+ * Latest Version : 0.5.7
  */
 public class AndKasirApp {
 
@@ -25,6 +30,21 @@ public class AndKasirApp {
      * @param args
      */
     public static void main(String[] args) {
+        
+        // fitur migrasi db
+        // untuk anda yang tidak mau repot
+        // dalam urusan database
+        if(args.length != 0){
+            // akan melakukan penyesuaian database yang dibutuhkan
+            // anda hanya tinggal menyeiapkan databse kosong
+            if (args[0].equals("db-migrate") || args[0].equals("-m")) {
+                MigrationManager.migrate();
+                System.exit(0);
+            } else if (args[0].equals("clean") || args[0].equals("-c")) {
+                MigrationManager.clean();
+                System.exit(0);
+            }
+        }
         // wrap dengan try catch untuk menghandle execption
         try{
             // standart penanganan backup database, setiap applikasi dipanggil, procedure
@@ -36,8 +56,9 @@ public class AndKasirApp {
         }catch(ExceptionInInitializerError e){
             // memeberi info pada user jika terjadi kesalahan/error
             JOptionPane.showMessageDialog(null
-                    ,"Kesalahan: File Konfigurasi tidak dapat ditemukan. Silahkan Baca README"
+                    ,"Kesalahan: File Konfigurasi atau database tidak dapat ditemukan. Silahkan Baca README"
                     ,"Error", JOptionPane.ERROR_MESSAGE);
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, e);
             // keluar dari aplikasi
             System.exit(0);
         }
